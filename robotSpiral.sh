42fc777b9575e2460954a591efd1eb2b9cc68a89
root <root@ip-172-31-21-29.us-west-1.compute.internal>
2021-12-20 12:02:00 -0800

    Updated from Central

--- robotSpiral.sh
+++
@@ -1,6 +0,0 @@
-# Java Example
-cd ./src
-javac -cp ../lib/robot.jar _02_code_flow/_2_robot_spiral/RobotSpiral.java 
-echo "compiled"
-cd ..
-java -cp "lib/robot.jar:src" -D _02_code_flow._2_robot_spiral.RobotSpiral
--- robotSquare.sh
+++
@@ -1,6 +0,0 @@
-# Java Example
-cd ./src
-javac -cp ../lib/robot.jar _02_code_flow/_1_robot_square/RobotSquare.java 
-echo "compiled"
-cd ..
-java -cp "lib/robot.jar:src" -D _02_code_flow._1_robot_square.RobotSquare
