0b1024960834090da24fcdf2def30becb360b052
league <league@192.168.1.85>
2025-12-05 16:56:29 -0800

    123

--- src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
+++ src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
@@ -18,21 +18,30 @@ public class AwesomeOrNot {
 		// 3. Print your variable to the console
 	System.out.println(x);
 		// 4. Get the user to enter something that they think is awesome
-	String liquid = JOptionPane.showInputDialog("Enter a type of liquid"); 
+	String liquid = JOptionPane.showInputDialog("Enter something you think is awesome");
 		// 5. If your variable is  0
-	if (x)
+	if(x == 0){
+		String test3 = JOptionPane.showInputDialog("SUPER COOL");
+	}
+	
 			// -- tell the user whatever they entered is awesome!
 	
 		// 6. If your variable is  1
-	
+	if(x == 1){
+		String test4 = JOptionPane.showInputDialog("awesome");
+	}
 			// -- tell the user whatever they entered is ok.
 	
 		// 7. If your variable is  2
-	
+	if(x == 2){
+		String test5 = JOptionPane.showInputDialog("ok");
+	}
 			// -- tell the user whatever they entered is boring.
 	
 		// 8. If your variable is  3
-	
+	if(x == 3){
+		String test6 = JOptionPane.showInputDialog("very boring");
+	}
 			// -- invent your own message to give to the user (be nice).
 	}
 }
